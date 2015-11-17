883dbc51dc3dce2e45e403d93bb097553294ac1e
sdhar1 <sdhar@cs.wisc.edu>
2015-11-16 22:02:20 -0600

    Added full functionality for the userInfo View controller. User can now logout and login from there.

--- Voyager/UserInfoViewController.swift
+++ Voyager/UserInfoViewController.swift
@@ -7,22 +7,101 @@
 //
 
 import UIKit
+import ParseFacebookUtilsV4
 import Parse
 
 class UserInfoViewController: UIViewController {
+    @IBOutlet weak var logInOutButton: MKButton!
 
-    @IBOutlet weak var userPhoto: UILabel!
+    @IBOutlet weak var userName: UILabel!
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func logInOut(sender: MKButton) {
+        if let _ = PFUser.currentUser()?.username {
+            PFUser.logOut()
+            userName.text = ""
+            logInOutButton.setTitle("Login using Facebook", forState: UIControlState.Normal)
+            let image = UIImage(named: "placeholder.jpg")
+            self.imageView.image = image
+        } else {
+            let permissions = ["public_profile"]
+            PFFacebookUtils.logInInBackgroundWithReadPermissions(permissions) {
+                (user: PFUser?, error: NSError?) -> Void in
+                if let error = error {
+                    print(error)
+                } else if let _ = user {
+                    let graphRequest = FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, gender"])
+                    graphRequest.startWithCompletionHandler({
+                        (connection,result,error) -> Void in
+                        
+                        if (error != nil) {
+                            print(error)
+                        } else if (result != nil) {
+                            PFUser.currentUser()?["gender"] = result["gender"]
+                            PFUser.currentUser()?["name"] = result["name"]
+                            
+                            let userId = result["id"] as! String
+                            let facebookProfilePictureUrl = "https://graph.facebook.com/\(userId)/picture?type=large"
+                            print(facebookProfilePictureUrl)
+                            if let fbpicUrl = NSURL(string: facebookProfilePictureUrl) {
+                                if let data = NSData(contentsOfURL: fbpicUrl) {
+                                    let imageFile = PFFile(data: data)
+                                    PFUser.currentUser()?["image"] = imageFile
+                                    PFUser.currentUser()?.saveInBackground()
+                                    
+                                }
+                            }
+                            
+                        }
+                    })
+                    
+                }
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpScreen()
     }
     
+    override func viewDidAppear(animated: Bool) {
+        
+        let image = UIImage(named: "placeholder.jpg")
+        self.imageView.image = image
+        if let _ = PFUser.currentUser()?.username {
+            
+            let name = PFUser.currentUser()?["name"] as! String
+            userName.text = name
+            
+            logInOutButton.setTitle("LOGOUT!", forState: UIControlState.Normal)
+            
+            let imageFile = PFUser.currentUser()?["image"] as! PFFile
+            imageFile.getDataInBackgroundWithBlock ({
+                (imageData: NSData?, error: NSError?) -> Void in
+                if error == nil {
+                    if let imageData = imageData {
+                        let imageFile = UIImage(data:imageData)
+                        self.imageView.image = imageFile
+                        
+                    }
+                }
+                }, progressBlock: {
+                    (percentDone: Int32) -> Void in
+                    print("progress = \(percentDone)")
+            })
+            
+        } else {
+            logInOutButton.setTitle("Login using Facebook!", forState: UIControlState.Normal)
+        }
+
+    }
     // MARK: - General
     
     func setUpScreen() {
-        userPhoto.font = UIFont.fontAwesomeOfSize(200)
-        userPhoto.text = String.fontAwesomeIconWithName(.User)
+        
     }
     
     /*
